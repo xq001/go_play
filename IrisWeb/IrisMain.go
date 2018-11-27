4bc70a59a76c4b717e41f70a2e674c3addf210be
admin <624491811@qq.com>
2018-11-27 11:44:55 +0800

    使用iris发布验证代理ip的服务

--- IrisWeb/IrisMain.go
+++ IrisWeb/IrisMain.go
@@ -5,20 +5,56 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/antchfx/htmlquery"
+	"github.com/go-ini/ini"
+	"github.com/kataras/iris"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"time"
 )
 
-func main() {
-	//VerifyProxy(prosy_list)
-	//ll := []string{"http://202.112.237.102:3128","http://202.112.237.112:3128"}
-	proxyList := VerifyProxy(getProxyIpList())
-	fmt.Println("验证后ip:", proxyList)
+var PublishUrl string
+var VerifyUrl string
+
+func init() {
+
+	config, err := ini.Load("./conf_for_iris.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+	PublishUrl = config.Section("BASE").Key("publish_url").String()
+	VerifyUrl = config.Section("BASE").Key("verify_url").String()
+
 }
-func getProxyIpList() []string {
+func main() {
+	app := iris.Default()
+	app.Get("/hello", Hello)
+	app.Get("/get_proxy", GetProxyHandle)
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{
+			"message": "pong",
+		})
+	})
+
+	// listen and serve on http://0.0.0.0:8080.
+	app.Run(iris.Addr(PublishUrl))
+}
+func GetProxyHandle(ctx iris.Context) {
+
+	proxyList := VerifyProxy(GetProxyIpList())
+	fmt.Println("验证后ip:", proxyList)
+	ctx.JSON(iris.Map{
+		"ip_list": proxyList,
+	})
+}
+
+func Hello(ctx iris.Context) {
+	ctx.Writef("hello iris")
+}
+func GetProxyIpList() []string {
 	//f, err := os.Open("./ne.html")
 	//if err != nil {
 	//	fmt.Println(err)
@@ -60,7 +96,7 @@ func VerifyProxy(urlList []string) []string {
 	var res []string
 	urls := urlList
 	for _, u := range urls {
-		go VerifyOneProxy(u, "https://maimai.cn/contact/interest_contact/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoxMTk4MjA4NDAsImxldmVsIjoyLCJ0IjoiY3R0In0.fnz6vNCb63n2j-Frr6H_vu1LuG1jgfoq2oPOITSAJdA", ch)
+		go VerifyOneProxy(u, VerifyUrl, ch)
 	}
 	for range urls {
 		i := <-ch
@@ -75,10 +111,10 @@ func VerifyOneProxy(verifyProxyUrl string, verifyUrl string, ch chan<- string) {
 	//验证一个代理
 	urli := url.URL{}
 	//go语言代理无论http或https都需要写成http
-	proxy_url, _ := urli.Parse(strings.Replace(verifyProxyUrl, "https", "http", 1))
+	proxyUrl, _ := urli.Parse(strings.Replace(verifyProxyUrl, "https", "http", 1))
 	client := http.Client{
 		Transport: &http.Transport{
-			Proxy:           http.ProxyURL(proxy_url),
+			Proxy:           http.ProxyURL(proxyUrl),
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		},
 		Timeout: time.Duration(5 * time.Second),
